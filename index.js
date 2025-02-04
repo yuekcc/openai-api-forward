05833cc3d622b068be2cfce5028123811125aa21
yuekcc <yuekcc@qq.com>
2025-02-04 14:49:54 +0800

    增加 / 、/v1/models 接口

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import CONFIG from './config.toml';
 
-console.log(`CWD: ${process.cwd()}`);
+function log(...args) {
+  console.log(...args);
+}
+
+function mask(str) {
+  return str.slice(0, 4).split('').filter(Boolean).join('') + '****' + str.slice(-4).split('').filter(Boolean).join('');
+}
 
 function verifyModelConfig(config) {
-  console.log('Verifying model config');
+  log('Verifying model config');
 
   if (!config.name) {
     throw new Error('Model name is missing');
@@ -15,29 +21,60 @@ function verifyModelConfig(config) {
     throw new Error('Model API token name is missing');
   }
 
-  console.log('>> name', config.name);
-  console.log('>> base_url', config.base_url);
-  console.log('>> token_name', config.token_name, process.env[config.token_name]);
+  log('>> name', config.name);
+  log('>> base_url', config.base_url);
+  log('>> token_name', config.token_name, mask(process.env[config.token_name]));
 }
 
 CONFIG.models.forEach(verifyModelConfig);
 
+function modelList() {
+  const data = {
+    data: CONFIG.models.map((model) => ({
+      object: 'model',
+      owned_by: 'true-openai',
+      id: model.name,
+    })),
+  };
+
+  return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
+}
+
+function serverStatus() {
+  const data = {
+    data: {
+      message: 'ok',
+    },
+  };
+  return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
+}
+
 const sve = Bun.serve({
   port: CONFIG.port ?? 10000,
+  static: {
+    '/favicon.ico': new Response('not found', { status: 404 }),
+  },
   async fetch(req) {
     const pathname = new URL(req.url).pathname;
-    const content = await req.json();
+    const method = req.method;
+    log(`[${method}] ${pathname}`);
 
+    if (pathname === '/') {
+      return serverStatus();
+    }
+
+    if (pathname === '/v1/models') {
+      return modelList();
+    }
+
+    const content = await req.json();
     const requiredModel = content.model;
     const config = CONFIG.models.find((model) => model.name === requiredModel);
 
-    const method = req.method;
     const baseUrl = config.base_url;
     const apiToken = process.env[config.token_name];
 
-    console.log(
-      `[${method}] ${pathname} => ${baseUrl}${pathname} for model ${requiredModel} with API token ${config.token_name}`,
-    );
+    log(`forward to ${baseUrl}${pathname} for ${requiredModel}`);
 
     const res = await fetch(`${baseUrl}${pathname}`, {
       method,
@@ -52,4 +89,5 @@ const sve = Bun.serve({
   },
 });
 
-console.log(`OpenAI API Forward Server running at http://localhost:${sve.port}`);
+log(`\nCWD: ${process.cwd()}`);
+log(`\nOpenAI API Forward Server running at http://localhost:${sve.port}`);
